7429b90636d2c78dc95426434c5243583b953dc2
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-03 02:03:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(calls)# code an argument here
   calls.collect{|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
 end
 
